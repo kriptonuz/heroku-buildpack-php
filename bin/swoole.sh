37eb6427709b2b79b31a3092cb3f560939999707
kriptonuz <kripton253@gmail.com>
2021-02-21 18:11:57 +0200

    comments

--- bin/swoole.sh
+++ bin/swoole.sh
@@ -3,26 +3,25 @@
 install_swoole_ext() {
   dep_url=git://github.com/swoole/swoole-src.git
   swoole_dir=swoole-src
-  echo "-----> Building Swoole..."
+  space="     "
+  echo "-----> Installing Swoole..."
   #
   ### Swoole
-  echo "[LOG] Downloading Swoole"
+  echo "$space Downloading Swoole"
   git clone $dep_url -q
   if [ ! -d "$swoole_dir" ]; then
     echo "[ERROR] Failed to find swoolephp directory $swoole_dir"
     exit
   fi
   cd $swoole_dir
-  #
-  #ln -s $BUILD_DIR/.heroku /app/.heroku
-  export PATH=/app/.heroku/php/bin:$PATH
 
+  echo "$space Building Swoole"
   {
     /app/.heroku/php/bin/phpize && \
     ./configure --enable-swoole-curl && \
     make && make install
   } &> /dev/null
   cd
-  echo "important extension swoole into php.ini"
+  echo "$swoole Adding swoole into php.ini"
   echo "extension=swoole.so" >> /app/.heroku/php/etc/php/php.ini
 }
