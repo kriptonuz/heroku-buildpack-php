f5284de8dd9b91889a7745a3b2a2e5dd320a3223
kriptonuz <kripton253@gmail.com>
2021-02-21 14:31:19 +0200

    log to stderr

--- bin/util/platform.php
+++ bin/util/platform.php
@@ -113,6 +113,7 @@ if(file_exists($COMPOSER_LOCK)) {
 				$sfr[$name] = getflag($lock["stability-flags"][$name]);
 			}
 		}
+		file_put_contents('php://stderr', print_r($sfr, true));
 		$require = array_merge($require, mkdep($sfr));
 	}
 	// same for platform-dev requirements, but they go into a require-dev section later, so only installs with --dev pull those in
